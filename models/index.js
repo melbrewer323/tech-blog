2e3e246f46242881acd9a8d29cee121fa4e05023
Melissa Brewer <melramirez727@gmail.com>
2021-08-05 13:49:32 -0500

    user/comment & user/post relationship

--- models/index.js
+++ models/index.js
@@ -2,10 +2,14 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+  });
 
 // Project.belongsTo(User, {
 //   foreignKey: 'user_id'
