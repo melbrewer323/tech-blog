97a1d3c31c79c10626fb88fe63cd9aca7c99ead5
Melissa Brewer <melramirez727@gmail.com>
2021-08-13 00:32:06 -0500

    Layout working, homepage working with navigation on top and conditional links shown correctly

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
-const { User, Blog, Comment } = require('../models');
-// const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Blog, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const blogData = await Blog.findAll({
       include: [
         {
           model: User,
-          attributes: ['username', 'id', 'email'],
+          attributes: ["username", "id", "email"],
         },
         {
           model: Comment,
@@ -18,62 +18,75 @@ router.get('/', async (req, res) => {
 
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    res.render('home', { 
-      blogs, 
-      logged_in: req.session.logged_in 
+    res.render("home", {
+      blogs,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get("/project/:id", async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-//     const project = projectData.get({ plain: true });
+    const project = projectData.get({ plain: true });
 
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render("project", {
+      ...project,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//       const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Project }],
+    });
 
-//     const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/login', (req, res) => {
-//   if (req.session.logged_in) {
-//     res.redirect('/profile');
-//     return;
-//   }
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
 
-//   res.render('login');
-// });
+  res.render("login", { hello: "hello" });
+});
+
+router.get("/dashboard", (req, res) => {
+  //operation to get logged in user
+  //fetch blogs b this user
+
+  try {
+   
+
+    res.render("dashboard", {   });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
